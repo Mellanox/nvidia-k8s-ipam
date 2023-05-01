7743bc844fa7667a28c966a64cc0b80aad2c87cc
adrianc <adrianc@nvidia.com>
2023-05-01 18:09:48 +0300

    Add types package
    types package contains CNI types
    for nv-ipam and host-local
    
    Signed-off-by: adrianc <adrianc@nvidia.com>

---
+++ pkg/cni/types/host-local.go
@@ -0,0 +1,64 @@
+/*
+ Copyright 2023, NVIDIA CORPORATION & AFFILIATES
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package types
+
+import (
+	"path/filepath"
+
+	"github.com/Mellanox/nvidia-k8s-ipam/pkg/cni/pool"
+)
+
+// TODO: do we want to support Routes ? DNS entires from ResolvConf as host-local CNI ?
+
+type HostLocalNetConf struct {
+	Name       string               `json:"name"`
+	CNIVersion string               `json:"cniVersion"`
+	IPAM       *HostLocalIPAMConfig `json:"ipam"`
+}
+
+type HostLocalIPAMConfig struct {
+	Type    string              `json:"type"`
+	DataDir string              `json:"dataDir"`
+	Ranges  []HostLocalRangeSet `json:"ranges"`
+}
+
+type HostLocalRangeSet []HostLocalRange
+
+type HostLocalRange struct {
+	RangeStart string `json:"rangeStart,omitempty"` // The first ip, inclusive
+	RangeEnd   string `json:"rangeEnd,omitempty"`   // The last ip, inclusive
+	Subnet     string `json:"subnet"`
+	Gateway    string `json:"gateway,omitempty"`
+}
+
+func HostLocalNetConfFromNetConfAndPool(nc *NetConf, p *pool.IPPoolConfig) *HostLocalNetConf {
+	return &HostLocalNetConf{
+		Name:       nc.Name,
+		CNIVersion: nc.CNIVersion,
+		IPAM: &HostLocalIPAMConfig{
+			Type:    "host-local",
+			DataDir: filepath.Join(nc.IPAM.DataDir, HostLocalDataDir),
+			Ranges: []HostLocalRangeSet{
+				[]HostLocalRange{
+					{
+						RangeStart: p.StartIP,
+						RangeEnd:   p.EndIP,
+						Subnet:     p.Subnet,
+						Gateway:    p.Gateway,
+					},
+				},
+			},
+		},
+	}
+}
---
+++ pkg/cni/types/types.go
@@ -0,0 +1,108 @@
+/*
+ Copyright 2023, NVIDIA CORPORATION & AFFILIATES
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+     http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/containernetworking/cni/pkg/types"
+	"k8s.io/client-go/kubernetes"
+
+	"github.com/Mellanox/nvidia-k8s-ipam/pkg/cni/k8sclient"
+)
+
+const (
+	DefaultConfDir    = "/etc/cni/net.d/nv-ipam.d"
+	DefaultKubeConfig = DefaultConfDir + "/" + "nv-ipam.kubeconfig"
+	DefaultDataDir    = "/var/lib/cni/nv-ipam"
+	DefaultLogFile    = "/var/log/nv-ipam-cni.log"
+
+	HostLocalDataDir = "state/host-local"
+	K8sNodeNameFile  = "k8s-node-name"
+)
+
+type IPAMConf struct {
+	types.IPAM
+
+	// PoolName is the name of the pool to be used to allocate IP
+	PoolName   string `json:"poolName"`
+	Kubeconfig string `json:"kubeconfig,omitempty"`
+	DataDir    string `json:"dataDir,omitempty"`
+	ConfDir    string `json:"confDir,omitempty"`
+	LogFile    string `json:"logFile,omitempty"`
+	LogLevel   string `json:"logLevel,omitempty"`
+
+	// internal configuration
+	NodeName  string
+	K8sClient *kubernetes.Clientset
+}
+
+type NetConf struct {
+	Name       string    `json:"name"`
+	CNIVersion string    `json:"cniVersion"`
+	IPAM       *IPAMConf `json:"ipam"`
+}
+
+func LoadConf(bytes []byte) (*NetConf, error) {
+	n := &NetConf{}
+
+	if err := json.Unmarshal(bytes, &n); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal configurations: %w", err)
+	}
+
+	if n.IPAM == nil {
+		return nil, fmt.Errorf("IPAM config missing 'ipam' key")
+	}
+
+	if n.IPAM.PoolName == "" {
+		return nil, fmt.Errorf("IPAM config missing poolName")
+	}
+
+	if n.IPAM.Kubeconfig == "" {
+		n.IPAM.Kubeconfig = DefaultKubeConfig
+	}
+
+	if n.IPAM.DataDir == "" {
+		n.IPAM.DataDir = DefaultDataDir
+	}
+
+	if n.IPAM.ConfDir == "" {
+		n.IPAM.ConfDir = DefaultConfDir
+	}
+
+	if n.IPAM.LogFile == "" {
+		n.IPAM.LogFile = DefaultLogFile
+	}
+
+	p := filepath.Join(n.IPAM.ConfDir, K8sNodeNameFile)
+	data, err := os.ReadFile(p)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read k8s node name from path: %s. %w", p, err)
+	}
+	n.IPAM.NodeName = strings.TrimSpace(string(data))
+	if n.IPAM.NodeName == "" {
+		return nil, fmt.Errorf("failed to parse k8s node name from path: %s", p)
+	}
+
+	n.IPAM.K8sClient, err = k8sclient.FromKubeconfig(n.IPAM.Kubeconfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create k8s client from kubeconfig path: %s. %w", n.IPAM.Kubeconfig, err)
+	}
+
+	return n, nil
+}
